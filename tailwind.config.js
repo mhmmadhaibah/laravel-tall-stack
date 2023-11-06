47552673af3074c94bdf22330e279e81ade081ae
Muhammad Haibah <mhmmadhaibah@gmail.com>
2023-11-07 01:23:18 +0700

    Set up a fresh TALL Stack app

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [forms, typography],
+}
